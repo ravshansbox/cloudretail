4657b0a4bf49798006aba89a03053e80b68f95ea
Ravshan Samandarov <ravshansbox@gmail.com>
2024-12-04 19:53:06 +0300

    set up http server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,14 @@
+import http from 'node:http';
 import pgMigrate from 'node-pg-migrate';
-import { DATABASE_URL, MIGRATIONS_DIR, MIGRATIONS_TABLE } from './constants';
-import { seed } from './seed';
+import {
+  DATABASE_URL,
+  HTTP_PORT,
+  MIGRATIONS_DIR,
+  MIGRATIONS_TABLE,
+} from './constants';
 import { pool } from './pool';
+import { seed } from './seed';
+import { registry } from './http';
 
 (async () => {
   await pgMigrate({
@@ -10,5 +17,15 @@ import { pool } from './pool';
     direction: 'up',
     migrationsTable: MIGRATIONS_TABLE,
   });
+
   await seed(pool);
+
+  const server = http.createServer();
+  server.on('request', (request, response) => {
+    registry.lookup(request, response);
+  });
+  server.on('listening', () => {
+    console.info(`Listening on port ${HTTP_PORT}`);
+  });
+  server.listen(HTTP_PORT);
 })().catch(console.error);
