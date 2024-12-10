587fd671b968460ecd75afc61812168f5ab8ea93
Ravshan Samandarov <ravshansbox@gmail.com>
2024-12-10 11:42:37 +0300

    set up `tailwind`

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,5 +1,20 @@
 import { FC } from 'react';
+import { classed, makeStrict } from '@tw-classed/react';
+
+const Button = makeStrict(
+  classed.button({
+    base: 'bg-emerald-400 px-4 py-2 text-white hover:bg-emerald-600',
+    variants: {
+      size: {
+        sm: 'text-sm',
+        md: 'text-md',
+        lg: 'text-lg',
+      },
+    },
+    defaultVariants: {},
+  }),
+);
 
 export const App: FC = () => {
-  return <div>Hello world</div>;
+  return <Button size="md">Hello World</Button>;
 };
