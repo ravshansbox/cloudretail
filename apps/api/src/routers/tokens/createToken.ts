71c44323d70bc324413ff1b07b7c63be3edab68a
Ravshan Samandarov <ravshansbox@gmail.com>
2024-12-23 11:27:17 +0300

    set up http server

---
+++ apps/api/src/routers/tokens/createToken.ts
@@ -0,0 +1,31 @@
+import { tokenDao } from '../../daos/tokens';
+import { userDao } from '../../daos/users';
+import { pool } from '../../pool';
+import zod from 'zod';
+import crypto from 'node:crypto';
+import { HttpError } from '../../exceptions';
+import express from 'express';
+
+const bodySchema = zod.object({
+  username: zod.string(),
+  password: zod.string(),
+});
+
+export const createToken = async (
+  request: express.Request,
+  response: express.Response,
+) => {
+  const body = bodySchema.parse(request.body);
+  const token = await f(body);
+  response.status(201).json(token);
+};
+
+const f = async (body) => {
+  const user = await userDao.selectByUsername(pool, {
+    username: body.username,
+  });
+  if (user.password !== crypto.hash('sha256', body.password)) {
+    throw new HttpError(401, 'Invalid username or password');
+  }
+  return tokenDao.createToken(pool, { userId: user.id });
+};
