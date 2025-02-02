ae96881dbac0784f2d2f1ada1726bfe07b3110d4
Ravshan Samandarov <ravshansbox@gmail.com>
2025-02-02 18:47:06 +0300

    [FE] move navbar from app to dashboard

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,14 +1,12 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AuthProvider } from './AuthContext';
-import { Navbar } from './components';
 import { Dashboard, Login } from './pages';
 
 export const App: FC = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/" Component={Dashboard} />
           <Route path="/login" Component={Login} />
