ee1c66541984d2b9a16500fdabfd9d23831da5f2
Ravshan Samandarov <ravshansbox@gmail.com>
2025-02-12 22:10:34 +0300

    configure `prettier` with `eslint`

--- apps/ui/eslint.config.mjs
+++ apps/ui/eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -30,4 +31,5 @@ export default tseslint.config(
       ],
     },
   },
+  eslintPluginPrettierRecommended,
 );
