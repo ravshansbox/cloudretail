6c039c8b76a7217e8de9de4ebb4f55c2a43736f4
Ravshan Samandarov <ravshansbox@gmail.com>
2025-02-23 17:38:04 +0300

    add `vitest`

---
+++ apps/api/src/setup.vitest.ts
@@ -0,0 +1,11 @@
+import { inject } from 'vitest';
+import { createHttpClient } from './test-utils.js';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var httpClient: ReturnType<typeof createHttpClient>;
+}
+
+beforeAll(() => {
+  global.httpClient = createHttpClient(inject('baseUrl'));
+});
