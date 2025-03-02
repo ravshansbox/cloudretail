c4c22baf76ebdc2d793c3fc786e26763ddfe6ad0
Ravshan Samandarov <ravshansbox@gmail.com>
2025-03-02 22:59:41 +0300

    [FE] implement `Table` component

--- apps/ui/src/components/Table.tsx
+++ apps/ui/src/components/Table.tsx
@@ -1,1 +1,32 @@
-export const Table = () => {};
+type Column<T> = {
+  title: string;
+  getData: (item: T) => string;
+};
+type TableProps<T> = {
+  items: T[] | null;
+  columns: Column<T>[];
+};
+export function Table<T>({ items, columns }: TableProps<T>) {
+  if (!items) return null;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column.title}>{column.title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index) => (
+          <tr key={index}>
+            {columns.map((column) => (
+              <td key={column.title}>{column.getData(item)}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
